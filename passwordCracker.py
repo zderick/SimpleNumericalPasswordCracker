605b5d89f28e82d9f1c2b1a94cef3dd067f0c1b7
zderick <centuryib100@gmail.com>
2022-03-21 17:32:09 -0700

    Create passwordCracker.py

---
+++ passwordCracker.py
@@ -0,0 +1,11 @@
+import pikepdf
+
+FILEPATH = "Desktop/.../myFile.pdf"
+
+for password in range(10000):
+    try:
+        with pikepdf.open(, password=str(password)):
+            print("Password found:", password)
+            break
+    except pikepdf._qpdf.PasswordError as e:
+        continue
